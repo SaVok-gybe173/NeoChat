fa759872e094130b5eb53f24e9207bf8298dcb95
DorotyDog <yaroslavdol88@gmail.com>
2026-08-10 16:56:10 +0400

    Create Session.hpp

---
+++ server/src/network/Session.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <thread>
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <string>
+#include "utils/Json.hpp"
+
+class Router;
+class Session : public std::enable_shared_from_this<Session> {
+    public:
+    using CloseCallback = std::function<void(std::shared_ptr<Session>)>;
+    Session(int socket, Router* router, CloseCallback onClose);
+    ~Session();
+    void start();
+    void stop();
+    bool deliver(const Json& message);
+    void setUsername(const std::string username);
+    std::string getUsername() const;
+    private:
+    void run();
+    bool readAll(uint8_t* buffer, size_t len);
+    bool sendJson(const std::string& jsonStr);
+    int socket_;
+    Router* router_;
+    std::thread thread_;
+    std::atomic<bool> running_;
+    CloseCallback onClose_;
+    std::mutex sendMutex_;
+    std::string username_;
+    mutable std::mutex usernameMutex_;
+};
