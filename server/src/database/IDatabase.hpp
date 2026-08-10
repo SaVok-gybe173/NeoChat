23dbeaec10033dc2656c6f06ed6935fc61b13d70
DorotyDog <yaroslavdol88@gmail.com>
2026-08-10 19:31:51 +0400

    Create IDatabase.hpp

---
+++ server/src/database/IDatabase.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <optional>
+
+struct User {
+    std::string username;
+    std::string passwordHash;
+    std::string salt;
+    std::string publicKey;
+};
+
+struct Message {
+    long long id;
+    std::string from;
+    std::string to;
+    std::string content;
+    long long timestamp;
+    bool encrypted = false;
+    std::string ephemeralKey;
+    std::string nonce;
+};
+
+class IDatabase {
+public:
+    virtual ~IDatabase() = default;
+    virtual bool init() = 0;
+
+    virtual bool addUser(const User& user) = 0;
+    virtual std::optional<User> getUser(const std::string& username) = 0;
+    virtual bool updateUserPublicKey(const std::string& username, const std::string& publicKey) = 0;
+    virtual std::optional<std::string> getUserPublicKey(const std::string& username) = 0;
+
+    virtual long long addMessage(const Message& msg) = 0;
+    virtual std::vector<Message> getMessages(const std::string& user1, const std::string& user2, int limit = 0, int offset = 0) = 0;
+    virtual std::vector<std::string> getAllUsers() = 0;
+};
