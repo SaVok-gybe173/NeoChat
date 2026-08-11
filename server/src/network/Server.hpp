6c2a015d6dd0c79863d005cd99d76c2cab1c5fdd
DorotyDog <yaroslavdol88@gmail.com>
2026-08-11 15:50:39 +0400

    Update Server.hpp

--- server/src/network/Server.hpp
+++ server/src/network/Server.hpp
@@ -4,29 +4,26 @@
 #include <unordered_map>
 #include <mutex>
 #include <memory>
-#include <vector>
-#include <thread>
-
 
 class Router;
 class Session;
 
 class Server {
-    public:
+public:
     Server(const std::string& host, int port, Router* router);
     ~Server();
     bool start();
     void stop();
     void run();
-    private:
+
+private:
     void removeSession(std::shared_ptr<Session> session);
+
     std::string host_;
     int port_;
     int listenSocket_;
     Router* router_;
     std::atomic<bool> running_;
-    std::unordered_map<int, std::shared_ptr<Session>> sessions_;
+    std::unordered_map<PlatformSocket, std::shared_ptr<Session>> sessions_;
     std::mutex sessionsMutex_;
-    std::vector<std::thread> reaperThreads_;
-    std::mutex reaperMutex_;
 };
