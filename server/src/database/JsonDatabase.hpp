f549df0e323b90d3ac78bbf3f11dfdf9a0b3b9c7
DorotyDog <yaroslavdol88@gmail.com>
2026-08-11 16:07:25 +0400

    Update JsonDatabase.hpp

--- server/src/database/JsonDatabase.hpp
+++ server/src/database/JsonDatabase.hpp
@@ -1,12 +1,19 @@
 #pragma once
+#include <vector>
+#include <optional>
 #include "database/IDatabase.hpp"
 #include "utils/Json.hpp"
 #include <mutex>
 #include <string>
+#include <atomic>
+#include <thread>
 
 class JsonDatabase : public IDatabase {
-    public:
-    JsonDatabase(const std::string& userFile, const std::string& messagesFile);
+public:
+    JsonDatabase(const std::string& usersFile, const std::string& messagesFile);
+    ~JsonDatabase();
+    JsonDatabase(const JsonDatabase&) = delete;
+    JsonDatabase& operator=(const JsonDatabase&) = delete;
     bool init() override;
     bool addUser(const User& user) override;
     std::optional<User> getUser(const std::string& username) override;
@@ -15,11 +22,13 @@ class JsonDatabase : public IDatabase {
     long long addMessage(const Message& msg) override;
     std::vector<Message> getMessages(const std::string& user1, const std::string& user2, int limit = 0, int offset = 0) override;
     std::vector<std::string> getAllUsers() override;
-    private:
+
+private:
     void loadUsers();
     void saveUsers();
     void loadMessages();
     void saveMessages();
+    void flush();
 
     std::string usersFile_;
     std::string messagesFile_;
@@ -27,4 +36,9 @@ class JsonDatabase : public IDatabase {
     Json messagesJson_;
     long long nextMsgId_ = 1;
     std::mutex mutex_;
+
+    std::atomic<bool> dirtyUsers_{false};
+    std::atomic<bool> dirtyMessages_{false};
+    std::atomic<bool> stopFlush_{false};
+    std::thread flushThread_;
 };
